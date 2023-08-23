0bd56d24fded35f254a8d8dd8813820bf12043b3
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-23 19:15:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ex---find-duplicate-nums
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tx9eme)
---
+++ index.js
@@ -0,0 +1,5 @@
+let arr = [3,5,6,8,8,10,10,14];
+
+let uniqueArr = [new Set(arr)];
+
+console.log(uniqueArr);
