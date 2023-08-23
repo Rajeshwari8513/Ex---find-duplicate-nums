0f833b0367a1c4a1afa7f5a8bde5d123a3485bff
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-23 19:31:15 +0530

    update

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let arr = [3,5,6,8,8,10,10,14];
 let uniqueArr = [new Set(arr)];
 
 console.log(uniqueArr);
+
+//  using loop
+
+// let newArr = [];
+
+// for(i=0; i<arr.length; i++){
+//     if(arr[i] !== arr[i-1]){
+//        newArr.push(arr[i]);
+//     }
+    
+// }
+
+// console.log(newArr);
